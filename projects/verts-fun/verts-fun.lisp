31584ac080e7ece30a84027e646d0af395d05510
renacava <36764359+renacava@users.noreply.github.com>
2023-05-17 23:59:19 +0200

    adjust-spacing & cam; clean

--- projects/verts-fun/verts-fun.lisp
+++ projects/verts-fun/verts-fun.lisp
@@ -57,9 +57,9 @@
   (setf (resolution (current-viewport))
         (get-cepl-context-surface-resolution))
   (update-viewport-perspective-matrix)
-  (setf *cam-pos* (v! (* 2 (+ 4 (* 5 (sin (* 2 (now))))))
-                      (* 2 (+ 6 (* 5 (sin (* 1.5 (now))))))
-                      (* 1 (+ 30 (sin (* 2 (now)))))))
+  (setf *cam-pos* (v! (* 2 (+ 18 (* 18 (sin (* 2 (now))))))
+                      (* 2 (+ 18 (* 18 (sin (* 1.5 (now))))))
+                      (* 1 (+ 90 (sin (* 2 (now)))))))
   (setf *cam-rot* (q:from-axis-angle (v! 0 1 0)
                                      (radians 0;;(* 10 (sin (* 5.0 (now))))
                                               )))
@@ -72,7 +72,7 @@
           :cam-rot (q:to-mat3 (q:inverse *cam-rot*))))
   (swap))
 
-(defun init (&optional (chunk-size 16) (chunk-height 16) (spacing 1.5))
+(defun init (&optional (chunk-size 16) (chunk-height 16) (spacing 5))
   (when t
     (when *buffer-stream*
       (free *buffer-stream*))
@@ -81,47 +81,13 @@
     (when (getf *gpu-array* :indices)
       (free (getf *gpu-array* :indices)))
     (setf *gpu-array* (cube-mesh-to-gpu-arrays (combine-cube-mesh-list (make-cool-chunk chunk-size chunk-height spacing))))
-    ;;(setf *gpu-array* (cube-mesh-to-gpu-arrays (make-cube-mesh)))
     (setf *buffer-stream* (make-buffer-stream (getf *gpu-array* :verts)
-                                              :index-array (getf *gpu-array* :indices)))
-    ))
+                                              :index-array (getf *gpu-array* :indices)))))
 
 (def-simple-main-loop play (:on-start (lambda () (init 16 16)))
   (draw))
 
-;; i think these are wrong. the depth is all messed up, i tried revesing each triplet but that didn't work.
-;; check online for a cube mesh and see its vert-indices list
-(defparameter *cube-mesh-indices* '(0 1 2 0 2 3 4 5 6 4 6 7 8 9 10 8 10 11 12 13 14 12 14 15 16 17 18 16 18 19 20
-                                    21 22 20 22 23))
-;; (defparameter *cube-mesh-indices* '(2 1 0 3 2 0 6 5 4 7 6 4 10 9 8 11 10 8 14 13 12 15 14 12 18 17 16 19 18 16 22
-;;                                     21 20 23 22 20))
 (defparameter *cube-mesh-data* (list
-                                (list (vec3 -0.5 -0.5 0.5) (vec3 0.0 0.0 1.0) (vec2 0.0 1.0))
-                                (list (vec3 0.5 -0.5 0.5) (vec3 0.0 0.0 1.0) (vec2 1.0 1.0))
-                                (list (vec3 0.5 0.5 0.5) (vec3 0.0 0.0 1.0) (vec2 1.0 0.0))
-                                (list (vec3 -0.5 0.5 0.5) (vec3 0.0 0.0 1.0) (vec2 0.0 0.0))
-                                (list (vec3 0.5 -0.5 -0.5) (vec3 0.0 0.0 -1.0) (vec2 0.0 1.0))
-                                (list (vec3 -0.5 -0.5 -0.5) (vec3 0.0 0.0 -1.0) (vec2 1.0 1.0))
-                                (list (vec3 -0.5 0.5 -0.5) (vec3 0.0 0.0 -1.0) (vec2 1.0 0.0))
-                                (list (vec3 0.5 0.5 -0.5) (vec3 0.0 0.0 -1.0) (vec2 0.0 0.0))
-                                (list (vec3 -0.5 -0.5 -0.5) (vec3 -1.0 0.0 0.0) (vec2 0.0 1.0))
-                                (list (vec3 -0.5 -0.5 0.5) (vec3 -1.0 0.0 0.0) (vec2 1.0 1.0))
-                                (list (vec3 -0.5 0.5 0.5) (vec3 -1.0 0.0 0.0) (vec2 1.0 0.0))
-                                (list (vec3 -0.5 0.5 -0.5) (vec3 -1.0 0.0 0.0) (vec2 0.0 0.0))
-                                (list (vec3 0.5 -0.5 0.5) (vec3 1.0 0.0 0.0) (vec2 0.0 1.0))
-                                (list (vec3 0.5 -0.5 -0.5) (vec3 1.0 0.0 0.0) (vec2 1.0 1.0))
-                                (list (vec3 0.5 0.5 -0.5) (vec3 1.0 0.0 0.0) (vec2 1.0 0.0))
-                                (list (vec3 0.5 0.5 0.5) (vec3 1.0 0.0 0.0) (vec2 0.0 0.0))
-                                (list (vec3 -0.5 0.5 0.5) (vec3 0.0 1.0 0.0) (vec2 0.0 1.0))
-                                (list (vec3 0.5 0.5 0.5) (vec3 0.0 1.0 0.0) (vec2 1.0 1.0))
-                                (list (vec3 0.5 0.5 -0.5) (vec3 0.0 1.0 0.0) (vec2 1.0 0.0))
-                                (list (vec3 -0.5 0.5 -0.5) (vec3 0.0 1.0 0.0) (vec2 0.0 0.0))
-                                (list (vec3 -0.5 -0.5 -0.5) (vec3 0.0 -1.0 0.0) (vec2 0.0 1.0))
-                                (list (vec3 0.5 -0.5 -0.5) (vec3 0.0 -1.0 0.0) (vec2 1.0 1.0))
-                                (list (vec3 0.5 -0.5 0.5) (vec3 0.0 -1.0 0.0) (vec2 1.0 0.0))
-                                (list (vec3 -0.5 -0.5 0.5) (vec3 0.0 -1.0 0.0) (vec2 0.0 0.0))))
-
-(defparameter *cube-mesh-data-x* (list
                                   (list (vec3 0.5 -0.5 0.5) (vec3 0.0 -1.0 0.0) (vec2 1.0 0.0))
                                   (list (vec3 0.5 -0.5 -0.5) (vec3 0.0 -1.0 0.0) (vec2 1.0 0.0))
                                   (list (vec3 0.5 0.5 -0.5) (vec3 0.0 -1.0 0.0) (vec2 1.0 0.0))
@@ -131,7 +97,7 @@
                                   (list (vec3 -0.5 0.5 -0.5) (vec3 0.0 -1.0 0.0) (vec2 1.0 0.0))
                                   (list (vec3 -0.5 0.5 0.5) (vec3 0.0 -1.0 0.0) (vec2 1.0 0.0))))
 
-(defparameter *cube-mesh-indices-x* '(4 0 3 4 3 7 0 1 2 0 2 3 1 5 6 1 6 2 5 4 7 5 7 6 7 3 2 7 2 6 0 5 1 0 4 5))
+(defparameter *cube-mesh-indices* '(4 0 3 4 3 7 0 1 2 0 2 3 1 5 6 1 6 2 5 4 7 5 7 6 7 3 2 7 2 6 0 5 1 0 4 5))
 ;; (defparameter *cube-mesh-indices-x* '(0 2 3 1 0 3))
 
 (defun make-cube-mesh (&key (size 1.0) (pos (vec3 0.0 0.0 0.0)) (vert-start-index 0))
@@ -144,17 +110,9 @@
                        *cube-mesh-data-x*)
         :indices (mapcar (lambda (x) (+ x vert-start-index))
                          *cube-mesh-indices-x*)))
-
 (defun cube-mesh-to-gpu-arrays (cube-mesh)
   (list :verts (make-gpu-array (getf cube-mesh :verts) :element-type 'g-pnt)
         :indices (make-gpu-array (getf cube-mesh :indices) :element-type :uint)))
-
-(defun combine-cube-meshes (cube1 cube2)
-  (list :verts (nconc (getf cube1 :verts)
-                      (getf cube2 :verts))
-        :indices (nconc
-                  (getf cube1 :indices)
-                  (getf cube2 :indices))))
 
 (defun combine-cube-mesh-list (cubes)
   (list :verts
@@ -163,15 +121,6 @@
         :indices
         (loop for cube in cubes
               nconc (getf cube :indices))))
-
-(defun make-cool-cubes (how-many &optional (spacing 1.0))
-  (let ((pos (vec3 0f0 0f0 0f0))) 
-    (combine-cube-mesh-list
-     (mapcar (lambda (x) (make-cube-mesh
-                          :pos (vec3 (+ (* x spacing) (aref pos 0))
-                                     0f0
-                                     0f0)))
-             (range how-many)))))
 
 (defun make-cool-chunk (width height &optional (spacing 1.2))
   (declare (fixnum width)
@@ -187,14 +136,10 @@
          (positions (mapcar (lambda (pos) (vec3 (* spacing (aref pos 0))
                                               (* spacing (aref pos 1))
                                               (* spacing (aref pos 2))))
-                            positions))
-         )
-;;    positions
+                            positions)))
     (loop for index below (length positions)
           collect (make-cube-mesh :pos (elt positions index)
-                                  :vert-start-index (* index 8;;4;;24
-                                                       )))
-    ))
+                                  :vert-start-index (* index 8)))))
 
 (defun block-at-pos-on-chunk-edge? (pos chunk-width chunk-height)
   "Returns t if the given pos corresponds to a block that's on the edge of the chunk."
